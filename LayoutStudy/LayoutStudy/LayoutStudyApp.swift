f03ccdb30546821c808b53ca183ae5c83467a777
Jason Stelzel <laptop@epigraf.com>
2022-08-19 13:19:53 -0700

    New Project LayoutStudy

---
+++ LayoutStudy/LayoutStudy/LayoutStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutStudyApp.swift
+//  LayoutStudy
+//
+//  Created by Jason Stelzel on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

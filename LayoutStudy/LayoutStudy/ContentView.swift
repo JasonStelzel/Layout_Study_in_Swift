7e9b824ae4126b51f170a9ade02cf898c290a9ad
Jason Stelzel <laptop@epigraf.com>
2022-08-19 13:40:38 -0700

    Basic .bordered Text, .bordered .frame, .width controlled by Slider

--- LayoutStudy/LayoutStudy/ContentView.swift
+++ LayoutStudy/LayoutStudy/ContentView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var proposedSize: CGSize = CGSize(width: 100, height: 100)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .border(Color.red)
+                .frame(width: proposedSize.width, height: proposedSize.height)
+                .border(Color.green)
+            Slider(value: $proposedSize.width, in: 0...300, label: { Text("Width")})
+            
+        }
     }
 }
 
